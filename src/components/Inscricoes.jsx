c3b7fcafebf45df4408686877f11bef56cd5c31d
bugkaue <alexkauezinho@gmail.com>
2024-10-13 18:19:19 -0300

    feat: exibição de eventos nas inscrições

--- src/components/Inscricoes.jsx
+++ src/components/Inscricoes.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { useFetchInscricoes } from '../services/Inscricao';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const Inscricoes = () => {
+  const { token, participante } = useAuth();
+  const { data: inscricoes, error: erroInscricoes } = useFetchInscricoes(token, participante?.id);
+
   return (
     <div className="inscricoes-container">
       <h2>Eventos em que você está inscrito</h2>
+
+      <div className="eventos-list">
+        {erroInscricoes && <p>Erro ao carregar as inscrições: {erroInscricoes.message}</p>}
+        {inscricoes?.length > 0 ? (
+          inscricoes.map((evento, index) => (
+            <div key={index} className="event-item">
+              <h3>{evento.titulo}</h3>
+              <p>{evento.descricao}</p>
+              <p>Data e Hora: {new Date(evento.dataHora).toLocaleString()}</p>
+              <p>Local: {evento.local}</p>
+              <p>Capacidade Máxima: {evento.capacidadeMaxima}</p>
+            </div>
+          ))
+        ) : (
+          <p>Você ainda não está inscrito em nenhum evento.</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/services/Inscricao.js
+++ src/services/Inscricao.js
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { useQuery } from '@tanstack/react-query';
 
 const subscribeEvento = async ({
   participanteId,
@@ -13,18 +14,39 @@ const subscribeEvento = async ({
   return response.data
 };
 
+const fetchInscricoes = async (token, participanteId) => {
+  const response = await axios.get(`https://localhost:7062/Inscricao/${participanteId}/eventos`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+
 export const useSubscribeEventos = (options ={}) => {
   return useMutation({
       mutationFn: ({eventoId, participanteId}) => subscribeEvento({
         eventoId,
         participanteId,
       }),
+
       onSuccess: (data) => {
           console.log("Inscrito no evento! ", data);
       },
       onError: (error) => {
           console.error("Erro ao se inscrever no evento! ", error);
       },
+      ...options
+  })
+}
+
+
+export const useFetchInscricoes = (token, participanteId, options ={}) => {
+  return useQuery({
+      queryKey: ["inscricoes", participanteId],
+      queryFn: () => fetchInscricoes(token, participanteId),
+      enabled: !!token && !!participanteId,
       ...options
   })
 }
